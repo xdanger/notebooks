45037aa43f9d87157c11bf6a2ff2d524c2f36a50
xdanger <xdanger@gmail.com>
2019-05-13 00:50:18 +0800

    Auto Backup notebooks

--- Coursera.MachineLearning.StanfordUniversity/machine-learning-ex1/ex1/computeCost.m
+++ Coursera.MachineLearning.StanfordUniversity/machine-learning-ex1/ex1/computeCost.m
@@ -13,7 +13,7 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-J = sum(power(theta' * X - y, 2));
+J = sum(power(theta' * X - y, 2)) / 2 * m;
 
 
 
