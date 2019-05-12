1c56dc8642b0f61d9ecb0f4cdfc20ef39293e706
xdanger <xdanger@gmail.com>
2019-05-13 00:44:16 +0800

    Auto Backup notebooks

--- Coursera.MachineLearning.StanfordUniversity/machine-learning-ex1/ex1/gradientDescent.m
+++ Coursera.MachineLearning.StanfordUniversity/machine-learning-ex1/ex1/gradientDescent.m
@@ -17,8 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-    theta = theta - alpha / m * (theta' * X)
+    for j = 1:2
+        theta(j) = theta(j) - alpha / m * (theta' * X - y)' * X[:, j]
+    end
     
 
 
